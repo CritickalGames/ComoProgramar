64a8535d4adf7ab092d70cafbe32fafd22b79021
CritickalGames <zgtale1@hotmail.com>
2022-04-16 01:04:15 -0300

    instancia

---
+++ Index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Reusar/css.css">
+
+    
+
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<!--CREAR NAV DE TIENDAS-->
+
+
+	<!--Contenido-->
+
+
+	<?php
+		include('SubDir/Indice/playList.php');
+	?>
+	<button onClick="back()">
+    BACK
+	</button>
+	<button onClick="next()">
+    NEXT
+	</button>
+
+
+
+
+
+<script type="text/javascript" src="Reusar/JavaScript/PlayList.js"></script>
+</body>
+</html>
